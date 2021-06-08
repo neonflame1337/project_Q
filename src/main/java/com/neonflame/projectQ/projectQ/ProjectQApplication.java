44a013313fa6efcaa469825ab5c22ea78245df00
neonflame1337 <neonflame.ua@gmail.com>
2021-06-08 23:30:04 +0300

    initial commit

---
+++ src/main/java/com/neonflame/projectQ/projectQ/ProjectQApplication.java
@@ -0,0 +1,13 @@
+package com.neonflame.projectQ.projectQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectQApplication.class, args);
+	}
+
+}
